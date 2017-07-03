c039796b6c40b9c72b98ec3807e3e685edcb4448
fono09 <fono@fono.jp>
2017-07-03 12:17:59 +0900

    2.56-8 complete

--- 2.56-8.scm
+++ 2.56-8.scm
@@ -40,6 +40,7 @@
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 (define (addend s) (cadr s))
+(define (auegend s) (caddr s))
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
@@ -96,25 +97,81 @@
       (error "unknown expression type -- DERIV" exp))))
 
 ; 2.57
-(define (make-sum a1 . a2)
-  (let 
-    ((a3 (list '+
-	      (fold + 0 (filter number? a2)) 
-	      (filter variable? a2))))
-    ((cond ((=number? a1 0) a3)
-           ((=number? a3 0) a1)
-           ((and (number? a1) (number? a3)) (+ a1 a3))
-	   (else (list '+ a1 a3))))))
+(define (make-sum a1 a2 . a3)
+  (let* 
+    (
+     (a4 (cons a1 (cons a2 a3)))
+     (a5 (fold + 0 (filter number? a4)))
+     (a6 (filter variable? a4))
+     (a7 (filter pair? a4))
+     (a8 (filter (lambda (x) (not (null? x))) (append a6 a7))))
+    (cond ((null? a8) a5)
+	  ((=number? a5 0) (if (> (length a8) 1) (cons '* a8) (car m8)))
+	  (else (cons '+ (cons a5 a8))))))
 
-(define (make-product m1 . m2)
-  (let 
-    ((m3 (if (member 0 m2) 0 
-	     (list '*
-		   (fold * 1 (filter number? m2))
-		   (let ((m4 (filter variable? m2)))
-		        ((if (pair? m4) m4 (car m4))))))))
-    ((cond ((or (=number? m1 0) (=number? m3 0)) 0)
-	   ((=number? m1 1) m3)
-	   ((=number? m3 1) m1)
-	   ((and (number? m1) (number? m3)) (* m1 m3))
-	   (else (list '* m1 m3))))))
+(define (make-product m1 m2 . m3)
+  (let*
+    (
+     (m4 (cons m1 (cons m2 m3)))
+     (m5 (fold * 1 (filter number? m4)))
+     (m6 (filter variable? m4))
+     (m7 (filter pair? m4))
+     (m8 (filter (lambda (x) (not (null? x))) (append m6 m7))))
+    (cond ((null? m8) m5)
+	  ((=number? m5 0) 0)
+	  ((=number? m5 1) (if (> (length m8) 1) (cons '* m8) (car m8)))
+	  (else (cons '* (cons m5 m8))))))
+
+
+(define (addend s) (cadr s))
+(define (augend s) (let ((s1 (cddr s)))(if (> (length s1) 1) (cons '+ s1) (car s1))))
+
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (let ((p1 (cddr p)))(if (> (length p1) 1) (cons '+ p1) (car p1))))
+
+
+; 2.58-a
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+(define (addend s) (car s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+(define (multiplier p) (car p))
+(define (multiplicand p) (caddr p))
+
+(define (make-sum a1 a2)
+  (cond  ((=number? a1 0) a2)
+	 ((=number? a2 0) a1)
+	 ((and (number? a1) (number? a2)) (+ a1 a2))
+	 (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+	(else (list m1 '* m2))))
+
+
+; 2.58-b
+
+(define (select-iter s p v)
+  (cond ((null? s) p)
+  	((eq? (car s) v) p) 
+    	(else (select-iter (cdr s) (append p (list (car s))) v))))
+
+(define (sum? x)
+  (memq '+ x))
+(define (addend s) 
+  (let ((s1 (select-iter s '() '+))) (if (> (length s1) 1) s1 (car s1))))
+(define (augend s)
+  (let ((s1 (cdr (memq '+ s)))) (if (> (length s1) 1) s1 (car s1))))
+
+(define (product? x)
+  (memq '* x))
+(define (multiplier p)
+  (let ((s1 (select-iter p '() '*))) (if (> (length s1) 1) s1 (car s1))))
+(define (multiplicand p)
+  (let ((p1 (cdr (memq '* p)))) (if (> (length p1) 1) p1 (car p1))))
